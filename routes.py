14964e9d4f6ddf67b123dbe3d4494fcbed3a82c1
whddk4415 <whddk4415@gmail.com>
2021-05-13 10:44:32 +0000

    convert location of garbege to lat and lng

--- routes.py
+++ routes.py
@@ -37,22 +37,26 @@ def find_all_closest():
     garbege_model = Garbege(client)
     lat = float(request.args.get("lat", 37.5666805))
     lng = float(request.args.get("lng", 126.9784147))
+    limit = request.args.get('limit', 10)
     closest_garbeges = garbege_model.find_all_closest(
         lat,
         lng,
         {
             "_id": 0,
             "name": 1,
-            "lat": 1,
-            "lng": 1,
+            "location.coordinates": 1,
             "hpid": 1,
             "is_official": 1,
         },
+        limit
     )
     
     for closest_garbege in closest_garbeges:
         closest_garbege['isOfficial'] = closest_garbege.pop('is_official')
-        
+        coordinates = closest_garbege.pop('location')['coordinates']
+        closest_garbege['lng'] = coordinates[0]
+        closest_garbege['lat'] = coordinates[1]
+    
     return {
         "msg": "success",
         "result": closest_garbeges,
